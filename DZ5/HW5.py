247916caad8f609aaad4c34f9ecf7f8e0d93b4e2
Makillaa <1999gmo@gmail.com>
2020-03-18 21:14:36 +0200

    add dict and rename variables

--- DZ5/HW5.py
+++ DZ5/HW5.py
@@ -1,11 +1,12 @@
 import requests  # Скачали пакет "Запроса"
 
 url = "https://httpbin.org/post"  # Ссылка, куда шлём запроса
-first_name = "Max"
-last_name = "Hryshchenko"
-email = "1@gmail.com"
-password = "1qwerty1"
-r = requests.post(url, {"first_name": first_name, "last_name": last_name,
-                        "email": email, "password": password})  # Запрос post с ключами и их значениями
-data = r.json()  # Реквест в виде Словаря
-print(data["form"])  # ???
+data = {
+    "first_name": "Max",
+    "last_name": "Hryshchenko",
+    "email": "1@gmail.com",
+    "password": "1qwerty1"
+}
+my_request = requests.post(url, data)  # Запрос post на сыылку и с ключами и их значениями
+data = my_request.json()  # Реквест в виде Словаря
+print(data["form"])  # Выводим данные записанные в форму
